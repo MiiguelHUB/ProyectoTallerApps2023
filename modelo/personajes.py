776a5ce13575d49932c592173b1c0ca0a576e309
Miguel Ortiz <108107421+MiiguelHUB@users.noreply.github.com>
2023-05-25 15:05:13 -0400

    Update personajes.py
    Se corrigio algunos errores menores. Se agregaron pruebas para verificar que los objetos estaban siendo construidos correctamente.

--- modelo/personajes.py
+++ modelo/personajes.py
@@ -1,51 +1,62 @@
 class Personaje:
-    def __init__(self, personaje_id, personaje_nombre, personaje_nivel, raza_id, estado_id, equipamiento_id, \
-    usuario_id):
-        self.personaje_id=personaje_id
-        self.personaje_nombre=personaje_nombre
-        self.personaje_nivel=personaje_nivel
-        self.raza_id=raza_id
-        self.estado_id=estado_id
-        self.equipamiento_id=equipamiento_id
-        self.usuario_id=usuario_id
+    def __init__(self, personaje_id, personaje_nombre, personaje_nivel, raza_id, estado_id, equipamiento_id, usuario_id):
+        self.personaje_id = personaje_id
+        self.personaje_nombre = personaje_nombre
+        self.personaje_nivel = personaje_nivel
+        self.raza_id = raza_id
+        self.estado_id = estado_id
+        self.equipamiento_id = equipamiento_id
+        self.usuario_id = usuario_id
 
-    def __str___(self):
+    def __str__(self):
         return """
         Personaje ID: {}
         Nombre personaje: {}
         Nivel: {}
-        raza ID: {}
+        Raza ID: {}
         Estado ID: {}
         Equipamiento ID: {}
-        usuario ID: {}""".format(self.personaje_id, self.personaje_nombre, self.personaje_nivel, self.raza_id, \
-        self.estado_id, self.equipamiento_id, self.usuario_id)
-    
+        Usuario ID: {}""".format(self.personaje_id, self.personaje_nombre, self.personaje_nivel, self.raza_id,
+                                self.estado_id, self.equipamiento_id, self.usuario_id)
+
+
 class Estado:
     def __init__(self, estado_id, estado_nombre):
-        self.estado_id=estado_id
-        self.estado_nombre=estado_nombre
-        
+        self.estado_id = estado_id
+        self.estado_nombre = estado_nombre
+
     def __str__(self):
-        return """" 
-        Estado ID: {} 
+        return """
+        Estado ID: {}
         Estado nombre: {}""".format(self.estado_id, self.estado_nombre)
-    
+
+
 class Equipamiento:
     def __init__(self, equipamiento_id, equipamiento_nombre):
-        self.equipamiento_id=equipamiento_id
-        self.equipamiento_nombre=equipamiento_nombre
-        
-    def __str__(self):
-        return """" 
-        Equipamiento ID: {} 
-        Equipamiento nombre: {}""".format(self.equipamiento_id, self.equipamiento_nombre)
-  
-"""llorar=Conex('localhost', 'root', 'python_db', 3306)
-cursor=llorar.getConex().cursor()
-cursor.execute("SELECT * FROM Doctor")
+        self.equipamiento_id = equipamiento_id
+        self.equipamiento_nombre = equipamiento_nombre
 
-for registro in cursor:
-    print(registro)"""
+    def __str__(self):
+        return """
+        Equipamiento ID: {}
+        Equipamiento nombre: {}""".format(self.equipamiento_id, self.equipamiento_nombre)
+
+
+if __name__ == "__main__":
+    # Pruebas de construcción de objetos
+
+    # Crear un objeto de la clase Estado
+    estado = Estado(1, "Vivo")
+    print(estado)
+
+    # Crear un objeto de la clase Equipamiento
+    equipamiento = Equipamiento(1, "Espada")
+    print(equipamiento)
+
+    # Crear un objeto de la clase Personaje
+    personaje = Personaje(1, "Gandalf", 50, 2, 1, 1, 1)
+    print(personaje)
+
 
 
     
